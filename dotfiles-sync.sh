f8a4826de146e72125cb051baa81eefef12ff67a
Betweenthelines <109682700+Betweenthelines@users.noreply.github.com>
2022-07-21 07:50:22 +0530

    Update dotfiles-sync.sh

--- dotfiles-sync.sh
+++ dotfiles-sync.sh
@@ -1,10 +1,13 @@
+
+mkdir -pv ~/.local/share/applications/rice
+
 cp terminalrc ~/.local/share/applications/rice/.terminalrc
 cp terminalrc ~/.terminalrc
 ##cp -r oh-my-zsh ~/.oh-my-zsh
+cp -r Pictures ~/Pictures
 cp -r config ~/.config
 cp -r urxvt  ~/.urxvt
 cp -r local ~/.local
-cp cleft ~/.cleft
 cp conkydark.sh ~/.conkydark.sh
 cp conkylight.sh ~/.conkylight.sh
 cp cright.sh ~/.cright.sh
@@ -16,4 +19,3 @@ cp light.sh ~/.light.sh
 cp mint.sh ~/.mint.sh
 cp themer.sh ~/.themer.sh
 cp Xresources ~/.Xresources
-
