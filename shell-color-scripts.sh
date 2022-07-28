c42526ffad5674b1ec906959975b1ac3ac12c6c9
Betweenthelines <109682700+Betweenthelines@users.noreply.github.com>
2022-07-28 22:17:23 +0530

    Update shell-color-scripts.sh

--- shell-color-scripts.sh
+++ shell-color-scripts.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ##credits to derek taylor #distrowatch
-git clone https://gitlab.com/dwt1/shell-color-scripts.git
+git clone https://gitlab.com/dwt1/shell-color-scripts.git 
 
 ##deleting ugly scripts imho
 
 shopt -s extglob
-cd ~/shell-color-scripts/colorscripts && rm -v !("crunchbang-mini"|"panes"|"hex"|"suckless")
+cd ~/ my-xfce4-dotfiles/shell-color-scripts/colorscripts && rm -v !("crunchbang-mini"|"panes"|"hex"|"suckless")
 shopt -u extglob
 
 ##installing scripts
