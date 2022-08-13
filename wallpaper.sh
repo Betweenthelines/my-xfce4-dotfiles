b77da0191e473ada181be8fe6f70cb8d39f48866
Betweenthelines <109682700+Betweenthelines@users.noreply.github.com>
2022-08-13 16:02:28 +0530

    Add files via upload

---
+++ wallpaper.sh
@@ -0,0 +1,38 @@
+echo "what is  property screen?"
+read first
+sed -i -e "s/screen*./$first/g" ~/.local/share/applications/rice/gruvbox-dark.sh
+sed -i -e "s/screen*./$first/g" ~/.local/share/applications/rice/Juno.sh
+sed -i -e "s/screen*./$first/g" ~/.local/share/applications/rice/light.sh
+sed -i -e "s/screen*./$first/g" ~/.local/share/applications/rice/mint.sh
+sed -i -e "s/screen*./$first/g" ~/.local/share/applications/rice/Kripton.sh
+sed -i -e "s/screen*./$first/g" ~/.local/share/applications/rice/Win95.sh
+sed -i -e "s/screen*./$first/g" ~/.local/share/applications/rice/dark.sh
+sed -i -e "s/screen*./$first/g" ~/.local/share/applications/rice/Robust.sh
+sed -i -e "s/screen*./$first/g" ~/.local/share/applications/rice/Nordic-Bluish.sh
+sed -i -e "s/screen*./$first/g" ~/.local/share/applications/rice/Nordic-polar.sh
+
+echo "what is  property monitor?"
+read second
+sed -i -e "s/monitore*./$second/g" ~/.local/share/applications/rice/gruvbox-dark.sh
+sed -i -e "s/monitore*./$second/g" ~/.local/share/applications/rice/Juno.sh
+sed -i -e "s/monitore*./$second/g" ~/.local/share/applications/rice/light.sh
+sed -i -e "s/monitore*./$second/g" ~/.local/share/applications/rice/mint.sh
+sed -i -e "s/monitore*./$second/g" ~/.local/share/applications/rice/Kripton.sh
+sed -i -e "s/monitore*./$second/g" ~/.local/share/applications/rice/Win95.sh
+sed -i -e "s/monitore*./$second/g" ~/.local/share/applications/rice/dark.sh
+sed -i -e "s/monitore*./$second/g" ~/.local/share/applications/rice/Robust.sh
+sed -i -e "s/monitore*./$second/g" ~/.local/share/applications/rice/Nordic-Bluish.sh
+sed -i -e "s/monitore*./$second/g" ~/.local/share/applications/rice/Nordic-polar.sh
+
+echo "what is  property workspace?"
+read third
+sed -i -e "s/workspace*./$third/g" ~/.local/share/applications/rice/gruvbox-dark.sh
+sed -i -e "s/workspace*./$third/g" ~/.local/share/applications/rice/Juno.sh
+sed -i -e "s/workspace*./$third/g" ~/.local/share/applications/rice/light.sh
+sed -i -e "s/workspace*./$third/g" ~/.local/share/applications/rice/mint.sh
+sed -i -e "s/workspace*./$third/g" ~/.local/share/applications/rice/Kripton.sh
+sed -i -e "s/workspace*./$third/g" ~/.local/share/applications/rice/Win95.sh
+sed -i -e "s/workspace*./$third/g" ~/.local/share/applications/rice/dark.sh
+sed -i -e "s/workspace*./$third/g" ~/.local/share/applications/rice/Robust.sh
+sed -i -e "s/workspace*./$third/g" ~/.local/share/applications/rice/Nordic-Bluish.sh
+sed -i -e "s/workspace*./$third/g" ~/.local/share/applications/rice/Nordic-polar.sh
