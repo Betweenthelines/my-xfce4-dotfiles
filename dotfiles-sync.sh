56e8a65e0d357b6923433a7adb484bf158442b4a
Betweenthelines <109682700+Betweenthelines@users.noreply.github.com>
2022-08-13 15:59:31 +0530

    Update dotfiles-sync.sh

--- dotfiles-sync.sh
+++ dotfiles-sync.sh
@@ -11,7 +11,7 @@ cp -r urxvt  ~/.urxvt
 cp -rT local ~/.local
 
 cp conkydark.sh ~/.conkydark.sh
-cp conkylight.sh ~/.conkylight.sh
+cp conkylight.sh ~/.conkydark.sh
 cp cright.sh ~/.cright.sh
 cp cleft.sh ~/.cleft.sh
 cp ftp.sh ~/.ftp.sh
@@ -22,3 +22,5 @@ cp light.sh ~/.light.sh
 cp mint.sh ~/.mint.sh
 cp themer.sh ~/.themer.sh
 cp Xresources ~/.Xresources
+cd && chmod +x .conkydark.sh .conkydark.sh .cright.sh .ftp.sh .ftp2.sh .gruvbox-dark.sh .dark.sh .light.sh .mint.sh  .themer.sh .Xresources 
+cd ~/.local/share/applications/rice/ && chmod +x $(ls)
