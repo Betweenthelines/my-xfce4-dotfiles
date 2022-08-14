826e9ceadcc2ab3675529ea179d0783b59966412
Betweenthelines <109682700+Betweenthelines@users.noreply.github.com>
2022-08-14 23:12:39 +0530

    Update whoami.sh

--- whoami.sh
+++ whoami.sh
@@ -6,4 +6,9 @@ sed -i -e 's/dreaming/'$first'/g' ~/.local/share/applications/light.desktop
 sed -i -e 's/dreaming/'$first'/g' ~/.local/share/applications/mint.desktop
 sed -i -e 's/dreaming/'$first'/g' ~/.local/share/applications/gruvbox-dark.desktop
 sed -i -e 's/dreaming/'$first'/g' ~/.local/share/applications/The-best.desktop
-
+sed -i -e 's/dreaming/'$first'/g' ~/.local/share/applications/comic.desktop
+sed -i -e 's/dreaming/'$first'/g' ~/.local/share/applications/Juno.desktop
+sed -i -e 's/dreaming/'$first'/g' ~/.local/share/applications/Kripton.desktop
+sed -i -e 's/dreaming/'$first'/g' ~/.local/share/applications/Nordic-bluish.desktop
+sed -i -e 's/dreaming/'$first'/g' ~/.local/share/applications/Nordic-polar.desktop
+sed -i -e 's/dreaming/'$first'/g' ~/.local/share/applications/Robust.desktop
