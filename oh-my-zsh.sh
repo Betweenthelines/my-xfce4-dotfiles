c6adbff007de282b0d59a53fee503cdd5f64d1bf
Betweenthelines <109682700+Betweenthelines@users.noreply.github.com>
2022-08-14 12:13:40 +0530

    Update oh-my-zsh.sh

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -1,17 +1,17 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended &&
 #sudo chsh $USER -s "/bin/zsh"
 ##zsh plugins installations
-git clone --depth 1 https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone --depth 1 https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting &&
 
-git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &&
 
-git clone --depth 1 https://github.com/zsh-users/zsh-completions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone --depth 1 https://github.com/zsh-users/zsh-completions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions &&
 
 ##sourcing zsh
 #. ~/.zshrc
 
 
 #cd ~/.oh-my-zsh/custom/plugins/
-sed -i -e 's/plugins=.*/plugins=(git zsh-completions zsh-autosuggestions fast-syntax-highlighting)/g' ~/.zshrc
+sed -i -e 's/plugins=.*/plugins=(git zsh-completions zsh-autosuggestions fast-syntax-highlighting)/g' ~/.zshrc &&
 
 chsh -s $(command -v zsh)
