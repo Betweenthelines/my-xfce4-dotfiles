b28d05b76b46e5361a2617cb89af862f2b4bb5db
Betweenthelines <109682700+Betweenthelines@users.noreply.github.com>
2022-08-14 11:57:11 +0530

    Update wallpaper.sh

--- wallpaper.sh
+++ wallpaper.sh
@@ -15,17 +15,17 @@ sed -i -e "s/screen*./$first/g" ~/.local/share/applications/rice/comic.sh
 
 echo "what is  property monitor?"
 read second
-sed -i -e "s/monitore*./$second/g" ~/.local/share/applications/rice/gruvbox-dark.sh
-sed -i -e "s/monitore*./$second/g" ~/.local/share/applications/rice/Juno.sh
-sed -i -e "s/monitore*./$second/g" ~/.local/share/applications/rice/light.sh
-sed -i -e "s/monitore*./$second/g" ~/.local/share/applications/rice/mint.sh
-sed -i -e "s/monitore*./$second/g" ~/.local/share/applications/rice/Kripton.sh
-sed -i -e "s/monitore*./$second/g" ~/.local/share/applications/rice/Win95.sh
-sed -i -e "s/monitore*./$second/g" ~/.local/share/applications/rice/dark.sh
-sed -i -e "s/monitore*./$second/g" ~/.local/share/applications/rice/Robust.sh
-sed -i -e "s/monitore*./$second/g" ~/.local/share/applications/rice/Nordic-Bluish.sh
-sed -i -e "s/monitore*./$second/g" ~/.local/share/applications/rice/Nordic-polar.sh
-sed -i -e "s/monitore*./$second/g" ~/.local/share/applications/rice/comic.sh
+sed -i -e "s/monitoreDP1/$second/g" ~/.local/share/applications/rice/gruvbox-dark.sh
+sed -i -e "s/monitoreDP1/$second/g" ~/.local/share/applications/rice/Juno.sh
+sed -i -e "s/monitoreDP1/$second/g" ~/.local/share/applications/rice/light.sh
+sed -i -e "s/monitoreDP1/$second/g" ~/.local/share/applications/rice/mint.sh
+sed -i -e "s/monitoreDP1/$second/g" ~/.local/share/applications/rice/Kripton.sh
+sed -i -e "s/monitoreDP1/$second/g" ~/.local/share/applications/rice/Win95.sh
+sed -i -e "s/monitoreDP1/$second/g" ~/.local/share/applications/rice/dark.sh
+sed -i -e "s/monitoreDP1/$second/g" ~/.local/share/applications/rice/Robust.sh
+sed -i -e "s/monitoreDP1/$second/g" ~/.local/share/applications/rice/Nordic-Bluish.sh
+sed -i -e "s/monitoreDP1/$second/g" ~/.local/share/applications/rice/Nordic-polar.sh
+sed -i -e "s/monitoreDP1/$second/g" ~/.local/share/applications/rice/comic.sh
 
 
 echo "what is  property workspace?"
