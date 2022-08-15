ff0c8924e5555d1d3897d0602e9ac17174b5fe90
Betweenthelines <109682700+Betweenthelines@users.noreply.github.com>
2022-08-15 11:06:59 +0530

    Update shell-color-scripts.sh

--- shell-color-scripts.sh
+++ shell-color-scripts.sh
@@ -6,15 +6,17 @@ git clone https://gitlab.com/dwt1/shell-color-scripts.git
 ##deleting ugly scripts imho
 
 shopt -s extglob
-cd ~/my-xfce4-dotfiles/shell-color-scripts/colorscripts && rm -v !("crunchbang-mini"|"panes"|"hex")
+cd ~/my-xfce4-dotfiles/shell-color-scripts/colorscripts && rm -v !("crunchbang-mini"|"panes")
 shopt -u extglob
 
 ##installing scripts
 
 cd ~/my-xfce4-dotfiles/shell-color-scripts/ && sudo make install &&
 sudo cp completions/_colorscript /usr/share/zsh/site-functions &&
-echo "colorscript random" ~/.zshrc
 
-
+echo "  " | tee -a ~/.zshrc
+echo "#" | tee -a ~/.zshrc
+echo "#" | tee -a ~/.zshrc
+echo "colorscript random" | tee -a ~/.zshrc
 
 
