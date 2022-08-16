3d04f0f0728555bc72dc35dd74f66b981bcb96cc
Betweenthelines <109682700+Betweenthelines@users.noreply.github.com>
2022-08-16 13:20:07 +0530

    Update onlythemechangerARCH.sh

--- onlythemechangerARCH.sh
+++ onlythemechangerARCH.sh
@@ -87,6 +87,11 @@ sed -i -e 's/screen0/'$screen'/g' ~/.local/share/applications/comic.desktop
 polymonitor=$(awk '/monitor/ {print $2}' ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml |  sed 's/name//g' |  sed 's/=//g' | sed 's/"//g' | sed 's/monitor0//g' | sed 's/monitor//g')
 echo $polymonitor
 sed -i -e 's/eDP1/'$polymonitor'/g' ~/.config/polybar/config.ini
+polybacklight=$(ls -1 /sys/class/backlight/)
+echo $polybacklight
+sed -i -e 's/intel_backlight/'$polybacklight'/g' ~/.config/polybar/config.ini
+
+
 
 ## now to install all the themes requirements
 sudo pacman -S xarchiver p7zip make qogir-gtk-theme-git qogir-icon-theme mint-themes mint-y-icons vimix-cursors vimix-gtk-themes vimix-icon-theme whitesur-gtk-theme arc-gtk-theme-git arc-solid-gtk-theme fluent-cursor-theme-git cutefish-icons nordic-darker-theme nordic-darker-theme lsd awesome-terminal-fonts ttf-iosevka-nerd xfce4-goodies papirus-icon-theme breeze-icons-git cantarell-fonts ttf-fira-code nerd-fonts-fira-code ttf-meslo-nerd-font-powerlevel10k ttf-meslo-nerd-font-powerlevel10k xterm polybar
@@ -96,7 +101,8 @@ echo '##xfwm4 themes hCREDITS TO addy-dclxvi ##'
 git clone https://github.com/addy-dclxvi/Xfwm4-Theme-Collections ~/.themes
 
 
-cd && 7z x ~/my-xfce4-dotfiles/themes.7z
+cd && 7z x ~/my-xfce4-dotfiles/themes.7z &&
+cd && 7z x ~/my-xfce4-dotfiles/icons.7z &&
 chmod +x ~/.config/polybar/launch.sh
 
 
