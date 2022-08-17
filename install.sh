0fb3a1655dc66f7fc8210401b84091570164ef5a
Betweenthelines <109682700+Betweenthelines@users.noreply.github.com>
2022-08-17 21:20:14 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,7 +32,12 @@ echo 'chaotic aur installed lets update pacman and download some theme, icons & 
 sudo pacman -Syyu
 echo '## fonts and gtk themes icons etc'
 sudo pacman -Rcns grml-zsh-config
-sudo pacman -S git curl wget make qogir-gtk-theme-git qogir-icon-theme scrot flameshot mint-themes mint-y-icons vimix-cursors vimix-gtk-themes vimix-icon-theme whitesur-gtk-theme arc-gtk-theme-git arc-solid-gtk-theme fluent-cursor-theme-git cutefish-icons nordic-darker-theme vi vim nordic-darker-theme lsd awesome-terminal-fonts ttf-iosevka-nerd xfce4-goodies papirus-icon-theme breeze-icons-git cantarell-fonts ttf-fira-code nerd-fonts-fira-code ttf-meslo-nerd-font-powerlevel10k ttf-meslo-nerd-font-powerlevel10k xterm
+
+
+# Let's install each package listed in the pkglist.txt file.
+sudo pacman --needed --ask 4 -Sy - < pkglist.txt || error "Failed to install required packages."
+
+
 
 echo '##xfwm4 themes https://github.com/addy-dclxvi/Xfwm4-Theme-Collections##' 
 git clone https://github.com/addy-dclxvi/Xfwm4-Theme-Collections ~/.themes
