7a9ffcabd97fd9f717a553de63edf2d0660dbabe
Betweenthelines <109682700+Betweenthelines@users.noreply.github.com>
2023-01-06 18:57:22 +0530

    Update onlythemechangerARCH.sh

--- onlythemechangerARCH.sh
+++ onlythemechangerARCH.sh
@@ -115,7 +115,12 @@ sed -i -e 's/intel_backlight/'$polybacklight'/g' ~/.config/polybar/config.ini
 
 
 # Let's install each package listed in the pkglist.txt file.
-sudo pacman -S xarchiver p7zip make qogir-gtk-theme-git qogir-icon-theme xcursor-breeze mint-themes mint-y-icons vimix-cursors vimix-gtk-themes vimix-icon-theme whitesur-gtk-theme arc-gtk-theme-git arc-solid-gtk-theme fluent-cursor-theme-git cutefish-icons nordic-darker-theme nordic-darker-theme lsd awesome-terminal-fonts ttf-iosevka-nerd xfce4-goodies papirus-icon-theme breeze-icons-git cantarell-fonts ttf-fira-code nerd-fonts-fira-code ttf-meslo-nerd-font-powerlevel10k ttf-meslo-nerd-font-powerlevel10k xterm polybar
+sudo pacman -S xarchiver p7zip make qogir-gtk-theme-git mpv qogir-icon-theme xcursor-breeze mint-themes mint-y-icons vimix-cursors vimix-gtk-themes vimix-icon-theme whitesur-gtk-theme arc-gtk-theme-git arc-solid-gtk-theme fluent-cursor-theme-git cutefish-icons nordic-darker-theme nordic-darker-theme lsd awesome-terminal-fonts ttf-iosevka-nerd xfce4-goodies papirus-icon-theme breeze-icons-git cantarell-fonts ttf-fira-code nerd-fonts-fira-code ttf-meslo-nerd-font-powerlevel10k ttf-meslo-nerd-font-powerlevel10k xterm polybar
+
+
+## mpv screenshot directory
+mkdir -pv ~/Pictures/mpv && cp -r /usr/share/doc/mpv/ ~/.config/
+echo 'screenshot-directory="~/Pictures/mpv/"' | tee -a ~/.config/mpv/mpv.conf
 
 
 echo '##xfwm4 themes hCREDITS TO addy-dclxvi ##' 
