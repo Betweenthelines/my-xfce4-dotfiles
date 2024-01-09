3ed177ee8e19e1d365b0d3ec5fca7a82b38ff53b
Betweenthelines <109682700+Betweenthelines@users.noreply.github.com>
2024-01-10 03:00:52 +0530

    Update Nordic-Bluish.sh

--- local/share/applications/rice/Nordic-Bluish.sh
+++ local/share/applications/rice/Nordic-Bluish.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-
-
-
 #xfce wallpaper#
-
-##killall -HUP xfdesktop
-
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitoreDP1/workspace0/last-image -s ~/Pictures/wall/FFXIII.png
-#feh --bg-fill  '~/Pictures/wall/10.png'
-
+SCREEN=$(xrandr --listactivemonitors | awk 'END {print $1}' | tr -d :)
+MONITOR=$(xrandr --listactivemonitors | awk -F ' ' 'END {print $2}' | tr -d \*+)
+#WALLPAPER
+xfconf-query --channel xfce4-desktop --property /backdrop/screen${SCREEN}/monitor${MONITOR}/workspace0/last-image --set ~/Pictures/wall/740758450.png
+~/Pictures/wall/FFXIII.png
 
 ##xfce icons
 xfconf-query -c xsettings -p /Net/IconThemeName -s Vimix-Black-dark
@@ -19,7 +15,6 @@ xfconf-query -c xsettings -p /Net/ThemeName -s Nordic-Polar-standard-buttons
 ##xfce cursor
 xfconf-query -c xsettings -p /Gtk/CursorThemeName -s Qogir-manjaro
 
-
 ##xfce Fonts
 xfconf-query -c xsettings -p /Gtk/FontName -s 'Source Code Pro for Powerline Semi-Bold Italic 12'
 
@@ -27,6 +22,7 @@ xfconf-query -c xsettings -p /Gtk/FontName -s 'Source Code Pro for Powerline Sem
 xfconf-query -c xfwm4 -p /general/theme -s Nordic-bluish-accent-v40
 xfconf-query -c xfwm4 -p /general/title_font -s "Sans Bold 10"
 
+## terminal theme since 2023
 xfconf-query -c xfce4-terminal -p /color-background -s 353C4A
 xfconf-query -c xfce4-terminal -p /color-foreground -s F8F8F2
 xfconf-query -c xfce4-terminal -p /color-cursor -s F8F8F2
