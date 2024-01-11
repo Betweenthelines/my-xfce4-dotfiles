eda2725e1acf06123ad2d19ba75b2a752e4bee7a
Betweenthelines <109682700+Betweenthelines@users.noreply.github.com>
2024-01-11 11:41:19 +0530

    Update whoami.sh

--- whoami.sh
+++ whoami.sh
@@ -20,54 +20,11 @@ sed -i -e 's/dreaming/'$first'/g' ~/.local/share/applications/rice/Everforest.de
 ##for keybindings
 sed -i -e 's/dreaming/'$first'/g' ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
 
-
-
-
-## for wallpapers to work in xfce4 environment
-monitor=$(awk '/monitor/ {print $2}' ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml |  sed 's/name//g' |  sed 's/=//g' | sed 's/"//g' | sed 's/monitor0//g')
-echo $monitor
-
-sed -i -e 's/monitoreDP1/'$monitor'/g' ~/.local/share/applications/rice/dark.sh
-sed -i -e 's/monitoreDP1/'$monitor'/g' ~/.local/share/applications/rice/light.sh
-sed -i -e 's/monitoreDP1/'$monitor'/g' ~/.local/share/applications/rice/mint.sh
-sed -i -e 's/monitoreDP1/'$monitor'/g' ~/.local/share/applications/rice/gruvbox-dark.sh
-sed -i -e 's/monitoreDP1/'$monitor'/g' ~/.local/share/applications/rice/Win95.sh
-sed -i -e 's/monitoreDP1/'$monitor'/g' ~/.local/share/applications/rice/comic.sh
-sed -i -e 's/monitoreDP1/'$monitor'/g' ~/.local/share/applications/rice/Juno.sh
-sed -i -e 's/monitoreDP1/'$monitor'/g' ~/.local/share/applications/rice/Kripton.sh
-sed -i -e 's/monitoreDP1/'$monitor'/g' ~/.local/share/applications/rice/Nordic-Bluish.sh
-sed -i -e 's/monitoreDP1/'$monitor'/g' ~/.local/share/applications/rice/Nordic-polar.sh
-sed -i -e 's/monitoreDP1/'$monitor'/g' ~/.local/share/applications/rice/Robust.sh
-sed -i -e 's/monitoreDP1/'$monitor'/g' ~/.local/share/applications/rice/comic.sh
-sed -i -e 's/monitoreDP1/'$monitor'/g'  ~/.local/share/applications/rice/true-dark.sh
-sed -i -e 's/monitoreDP1/'$monitor'/g' ~/.local/share/applications/rice/mint-light.sh
-sed -i -e 's/monitoreDP1/'$monitort'/g' ~/.local/share/applications/rice/Everforest.sh
-
-
-screen=$(awk '/screen/ {print $2}' ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml | sed 's/name=//g' | sed 's/"//g')
-echo $screen
-
-sed -i -e 's/screen0/'$screen'/g' ~/.local/share/applications/dark.desktop
-sed -i -e 's/screen0/'$screen'/g' ~/.local/share/applications/light.desktop
-sed -i -e 's/screen0/'$screen'/g' ~/.local/share/applications/mint.desktop
-sed -i -e 's/screen0/'$screen'/g' ~/.local/share/applications/gruvbox-dark.desktop
-sed -i -e 's/screen0/'$screen'/g' ~/.local/share/applications/The-best.desktop
-sed -i -e 's/screen0/'$screen'/g' ~/.local/share/applications/comic.desktop
-sed -i -e 's/screen0/'$screen'/g' ~/.local/share/applications/Juno.desktop
-sed -i -e 's/screen0/'$screen'/g' ~/.local/share/applications/Kripton.desktop
-sed -i -e 's/screen0/'$screen'/g' ~/.local/share/applications/Nordic-bluish.desktop
-sed -i -e 's/screen0/'$screen'/g' ~/.local/share/applications/Nordic-polar.desktop
-sed -i -e 's/screen0/'$screen'/g' ~/.local/share/applications/Robust.desktop
-sed -i -e 's/screen0/'$screen'/g' ~/.local/share/applications/comic.desktop
-sed -i -e 's/screen0/'$screen'/g'  ~/.local/share/applications/rice/true-dark.sh
-sed -i -e 's/screen0/'$screen'/g' ~/.local/share/applications/rice/mint-light.sh
-sed -i -e 's/screen0/'$screen'/g' ~/.local/share/applications/rice/Everforest.sh
-
-
 ## for polybar to work properly
-polymonitor=$(awk '/monitor/ {print $2}' ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml |  sed 's/name//g' |  sed 's/=//g' | sed 's/"//g' | sed 's/monitor0//g' | sed 's/monitor//g')
+polymonitor=$(xrandr --listactivemonitors | awk -F ' ' 'END {print $2}' | tr -d \*+)
 echo $polymonitor
 sed -i -e 's/eDP1/'$polymonitor'/g' ~/.config/polybar/config.ini
+
 polybacklight=$(ls -1 /sys/class/backlight/)
 echo $polybacklight
 sed -i -e 's/intel_backlight/'$polybacklight'/g' ~/.config/polybar/config.ini
