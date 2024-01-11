76573fe3a525ca25891679ad9bf7c588b863390b
Betweenthelines <109682700+Betweenthelines@users.noreply.github.com>
2024-01-11 11:47:11 +0530

    Update shell-color-scripts.sh

--- shell-color-scripts.sh
+++ shell-color-scripts.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
+cp panes.sh ~/.config/xfce4/terminal/
 
-##credits to derek taylor #distrowatch
-git clone https://gitlab.com/dwt1/shell-color-scripts.git 
-
-##deleting ugly scripts imho
-
-shopt -s extglob
-cd ~/my-xfce4-dotfiles/shell-color-scripts/colorscripts && rm -v !("crunchbang-mini"|"panes")
-shopt -u extglob
-
-##installing scripts
-
-cd ~/my-xfce4-dotfiles/shell-color-scripts/ && sudo make install &&
-sudo cp completions/_colorscript /usr/share/zsh/site-functions &&
-
-echo "  " | tee -a ~/.zshrc
+echo "#colorscript pane only" | tee -a ~/.zshrc
+echo "~/.config/xfce4/terminal/panes.sh" | tee -a ~/.zshrc
 echo "#" | tee -a ~/.zshrc
-echo "#" | tee -a ~/.zshrc
-echo "colorscript random" | tee -a ~/.zshrc
-
-
